2f15e0f2df7a98749337d07fcf03348bfdc80c15
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-08-27 14:22:59 +0530

    Add files via upload

---
+++ type conversions.py
@@ -0,0 +1,41 @@
+''' FORMAT SPECIFIERS 25TH AUG '''
+"""
+
+int --> %d  or %I
+float-->  %float
+str--> %s
+"""
+# a=5; b=6.7; c='hello'
+# print(type(a),type(b),type(c))
+# print(str(a)+c)
+# print(a)
+# print('%d' %a)
+# print('%i' %a)
+# print(b)
+# print('%f' %b)
+# print('%.1f' %b)
+# a=5; b=6.7; c='hello'
+# print(float(a))
+# print(str(a))
+# print('%f' %a)
+# print('%.1f' %a)
+a=5; b=6.7; c='hello'
+# print(b)
+# print(int(b))
+# print(str(b))
+
+# c='hello'
+# print(int(c))
+# print(float(c))
+# d='5'
+# print(int(d))
+# print(float(d))
+e='5.7'
+print(float(e))
+g=float(e)
+print(int(g))
+
+
+
+
+
